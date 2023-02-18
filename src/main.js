39853248e45af8706f863547e9d8bb4899bed8af
denypostolachi <deny.postolachi88@gmail.com>
2023-02-18 23:02:50 +0200

    init

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import "@/css/normalize.css";
+import "@/css/style.css";
+
 
 createApp(App).use(store).use(router).mount('#app')
