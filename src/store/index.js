dffa028db6fbb2ce241068b49825026132b85a85
denypostolachi <deny.postolachi88@gmail.com>
2023-02-20 01:37:34 +0200

    style fixies

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { createStore } from "vuex";
-import partners from "@/store/modules/partners";
 import cart from "@/store/modules/cart";
 
 export default createStore({
   modules: {
-    partners,
     cart,
   },
 });
